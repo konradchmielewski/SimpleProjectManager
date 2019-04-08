6cfc9db87fa13515283d7e1ce19ee4494cc3d3f5
Konrad Chmielewski <konradchmielewski.moe@gmail.com>
2019-04-08 15:39:37 +0200

    Add project files.

---
+++ SimpleProjectManager.Module/BusinessObjects/SimpleProjectManagerDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Data.Entity;
+using System.Data.Common;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.ComponentModel;
+using DevExpress.ExpressApp.EF.Updating;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EF.DesignTime;
+using SimpleProjectManager.Module.BusinessObjects.Marketing;
+
+namespace SimpleProjectManager.Module.BusinessObjects {
+	public class SimpleProjectManagerContextInitializer : DbContextTypesInfoInitializerBase {
+		protected override DbContext CreateDbContext() {
+			return new SimpleProjectManagerDbContext("App=EntityFramework");
+		}
+	}
+	[TypesInfoInitializer(typeof(SimpleProjectManagerContextInitializer))]
+	public class SimpleProjectManagerDbContext : DbContext {
+		public SimpleProjectManagerDbContext(String connectionString)
+			: base(connectionString) {
+		}
+		public SimpleProjectManagerDbContext(DbConnection connection)
+			: base(connection, false) {
+		}
+		public SimpleProjectManagerDbContext()
+			: base("name=ConnectionString") {
+		}
+		public DbSet<ModuleInfo> ModulesInfo { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Testimonial> Testimonial { get; set; }
+    }
+}
---
+++ SimpleProjectManager.Module/DatabaseUpdate/Updater.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using DevExpress.ExpressApp;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Persistent.BaseImpl;
+using SimpleProjectManager.Module.BusinessObjects.Marketing;
+using SimpleProjectManager.Module.BusinessObjects.Planning;
+
+namespace SimpleProjectManager.Module.DatabaseUpdate {
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppUpdatingModuleUpdatertopic.aspx
+    public class Updater : ModuleUpdater {
+        public Updater(IObjectSpace objectSpace, Version currentDBVersion) :
+            base(objectSpace, currentDBVersion) {
+        }
+        public override void UpdateDatabaseAfterUpdateSchema()
+        {
+            base.UpdateDatabaseAfterUpdateSchema();
+            if (ObjectSpace.CanInstantiate(typeof(Project)))
+            {
+                Project project = ObjectSpace.FindObject<Project>(
+                    new BinaryOperator("Name", "DevExpress XAF Features Overview"));
+                if (project == null)
+                {
+                    project = ObjectSpace.CreateObject<Project>();
+                    project.Name = "DevExpress XAF Features Overview";
+                }
+                ObjectSpace.CommitChanges();
+            }
+            if (ObjectSpace.CanInstantiate(typeof(Customer)))
+            {
+                Customer customer = ObjectSpace.FindObject<Customer>(
+                    CriteriaOperator.Parse("FirstName == 'Ann' && LastName == 'Devon'"));
+                if (customer == null)
+                {
+                    customer = ObjectSpace.CreateObject<Customer>();
+                    customer.FirstName = "Ann";
+                    customer.LastName = "Devon";
+                    customer.Company = "Eastern Connection";
+                }
+                ObjectSpace.CommitChanges();
+            }
+        }
+        public override void UpdateDatabaseBeforeUpdateSchema() {
+            base.UpdateDatabaseBeforeUpdateSchema();
+            //if(CurrentDBVersion < new Version("1.1.0.0") && CurrentDBVersion > new Version("0.0.0.0")) {
+            //    RenameColumn("DomainObject1Table", "OldColumnName", "NewColumnName");
+            //}
+        }
+    }
+}
